aa6409c242729da9449df762e19d2bd2999671f4
LikeSwap <likeswaporg@gmail.com>
2021-12-27 19:25:36 +0700

    upload

---
+++ abi/precache-manifest.957562c9d627c5c5bf504518a5ae33b8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d802b88f07564eef040b9b9c2dce773d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ba3718893b3fc83439fc",
+    "url": "./static/js/2.6936fe6c.chunk.js"
+  },
+  {
+    "revision": "4b160f0280cfde1e96e5745cb958d85e",
+    "url": "./static/js/2.6936fe6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce60f6a0292310dee258",
+    "url": "./static/js/main.d197c595.chunk.js"
+  },
+  {
+    "revision": "433e78278fbca96394bb",
+    "url": "./static/js/runtime-main.1eaa6f7e.js"
+  }
+]);
